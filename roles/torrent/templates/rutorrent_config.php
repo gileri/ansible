437c3f83624636009f3be10c0180eb32c196ab45
gileri <e+git8413@linuxw.info>
2020-06-27 13:15:01 +0200

    Add https, web, nas, tor, torrent roles

---
+++ roles/torrent/templates/rutorrent_config.php
@@ -0,0 +1,72 @@
+<?php
+	// configuration parameters
+
+	// for snoopy client
+	@define('HTTP_USER_AGENT', 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0', true);
+	@define('HTTP_TIME_OUT', 30, true);	// in seconds
+	@define('HTTP_USE_GZIP', true, true);
+	$httpIP = null;				// IP string. Or null for any.
+	$httpProxy = array
+	(
+		'use' 	=> false,
+		'proto'	=> 'http',		// 'http' or 'https'
+		'host'	=> 'PROXY_HOST_HERE',
+		'port'	=> 3128
+	);
+
+	@define('RPC_TIME_OUT', 5, true);	// in seconds
+
+	@define('LOG_RPC_CALLS', false, true);
+	@define('LOG_RPC_FAULTS', true, true);
+
+	// for php
+	@define('PHP_USE_GZIP', false, true);
+	@define('PHP_GZIP_LEVEL', 2, true);
+
+	$schedule_rand = 10;			// rand for schedulers start, +0..X seconds
+
+	$do_diagnostic = true;
+	$log_file = '/tmp/errors.log';		// path to log file (comment or leave blank to disable logging)
+
+	$saveUploadedTorrents = true;		// Save uploaded torrents to profile/torrents directory or not
+	$overwriteUploadedTorrents = false;     // Overwrite existing uploaded torrents in profile/torrents directory or make unique name
+
+	$topDirectory = '{{ torrent_top_directory }}';			// Upper available directory. Absolute path with trail slash.
+	$forbidUserSettings = true;
+
+	$scgi_port = 0;
+	$scgi_host = "unix://{{ rtorrent_var_dir }}/session/rpc.socket";
+
+	// For web->rtorrent link through unix domain socket 
+	// (scgi_local in rtorrent conf file), change variables 
+	// above to something like this:
+	//
+	// $scgi_port = 0;
+	// $scgi_host = "unix:///tmp/rpc.socket";
+
+	$XMLRPCMountPoint = "/RPC2";		// DO NOT DELETE THIS LINE!!! DO NOT COMMENT THIS LINE!!!
+
+    // For some reason they are not detected from path
+	$pathToExternals = array(
+		"php" => '/usr/bin/php',
+		"python" => '/usr/bin/python',
+		"mediainfo" => '/usr/bin/mediainfo',
+		"curl" => '/usr/bin/curl',
+		"pgrep" => '/usr/bin/pgrep',
+	);
+
+	$localhosts = array( 			// list of local interfaces
+		"127.0.0.1",
+		"localhost",
+	);
+
+	$profilePath = '/var/lib/rutorrent/';		// Custom path outside systemd's ProtectSystem=full protection
+	$profileMask = 0777;			// Mask for files and directory creation in user profiles.
+						// Both Webserver and rtorrent users must have read-write access to it.
+						// For example, if Webserver and rtorrent users are in the same group then the value may be 0770.
+
+	$tempDirectory = null;			// Temp directory. Absolute path with trail slash. If null, then autodetect will be used.
+
+	$canUseXSendFile = false;		// If true then use X-Sendfile feature if it exist
+
+	$locale = "UTF8";
